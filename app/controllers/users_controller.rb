c3997868766b75aa949eccd6c8c9c1ff39e2c8cc
Matib <matib05@gmail.com>
2017-07-19 21:59:35 +0000

    add sessions test and controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,4 @@
 class UsersController < ApplicationController
-    
     def new
         @user = User.new
     end
@@ -13,11 +12,11 @@ class UsersController < ApplicationController
        
        if @user.save
            flash[:notice] = "Welcome to Bloccit, #{@user.name}!"
+           create_session(@user)
            redirect_to(root_path)
        else
            flash.now[:alert] = "There was an error creating your account. Please try again."
            render(:new)
        end
     end
-    
 end
