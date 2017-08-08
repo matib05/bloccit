f44235250a56377894cc36a8b3f354b9e5f14886
Matib <matib05@gmail.com>
2017-08-08 21:18:29 +0000

    assigning authrization roles

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,2 +1,7 @@
 module PostsHelper
+    
+    def is_user_authorized_for_post?(post)
+        current_user && (current_user == post.user || current_user.admin?)
+    end
+    
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170731195019) do
+ActiveRecord::Schema.define(version: 20170803043750) do
 
   create_table "advertisements", force: :cascade do |t|
     t.string   "title"
@@ -96,6 +96,7 @@ ActiveRecord::Schema.define(version: 20170731195019) do
     t.string   "password_digest"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+    t.integer  "role"
   end
 
 end
